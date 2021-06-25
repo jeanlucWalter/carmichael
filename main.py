042084e66c34cffdad6f003670b5cb818cd1568d
jeanluc <jeanluc.walter@fantasiapp.com>
2021-06-25 16:04:59 +0200

    ending main big prime Mersenne

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class CarmichaelNumber:
   listAllPrimes = [3, 5]
 
   def __init__(self) -> None:
-    self.__max = 0
+    self.__max = 100000000
 
   def computeAllCarmichael(self, max:int) -> None:
     self.__max, start = max, time.time()
@@ -15,10 +15,30 @@ class CarmichaelNumber:
         print(isCarmichael)
     print("delay is {} sec".format('{:.2f}'.format(time.time() - start)))
 
-  def __testCarmichael(self, isCarmichael) -> bool:
+  def computeBigMersenne(self, numberOfMersennePrimes:int):
+    listMersennePrimes= [(1, 2, 3), (2, 3, 7), (3, 5, 31)]
+    while len(listMersennePrimes) <= numberOfMersennePrimes:
+      prime, mersenne = self.__computeNextMersennePrime()
+      listMersennePrimes.append((len(listMersennePrimes), prime, mersenne))
+      print("le nombre premier de Mersenne de numéro {} est associé au nombre premier {} et contient {} chiffres".format(len(listMersennePrimes), prime, len(str(mersenne))))
+      print("il vaut : ", mersenne)
+      print()
+      
+    
+  def __computeNextMersennePrime(self):
+    next = None
+    while True:
+      next = CarmichaelNumber.listAllPrimes[-1] + 2 if next == None else next + 2
+      listFactors = self.__computeFactors(next, [], CarmichaelNumber.listAllPrimes)
+      if len(listFactors) == 1:
+        prime, mersenne = listFactors[0], 2 ** listFactors[0] - 1
+        if self.testLucasLhemer(prime, mersenne):
+          return [prime, mersenne]
+
+  def __testCarmichael(self, isCarmichael:int) -> bool:
     if not self.testFermat(isCarmichael):
       return False
-    setFactors = set(self.__computeFactors(isCarmichael, [], self.listAllPrimes))
+    setFactors = set(self.__computeFactors(isCarmichael, [], CarmichaelNumber.listAllPrimes))
     if len(setFactors) < 3:
       return False
     for prime in setFactors:
@@ -60,6 +80,13 @@ class CarmichaelNumber:
           expTwo -= 1
       return False
 
+  def testLucasLhemer(self, prime:int, mersenne:int):
+    index, suiteValue = 1, 4
+    while index < prime - 1:
+      suiteValue = (suiteValue ** 2 - 2) % mersenne
+      index += 1
+    return suiteValue == 0
+
   def __computeExpTwo(self, numberTested:int, exp:int = 0) -> int:
     if numberTested % 2:
       return exp
@@ -74,4 +101,4 @@ class CarmichaelNumber:
 
 
 object = CarmichaelNumber()
-object.computeAllCarmichael(1000000)
+object.computeBigMersenne(21)
